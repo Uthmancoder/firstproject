bc834ea97501e58e7a7a444a20f6e10fea448b32
Uthmancoder <akandeadebayo025@gmail.com>
2024-05-20 18:01:23 +0100

    20-05-2024 Monday class

--- src/app/homepage/homepage.component.ts
+++ src/app/homepage/homepage.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
+import { ProductsComponent } from '../products/products.component';
 @Component({
   selector: 'app-homepage',
   standalone: true,
-  imports: [CommonModule],
+  imports: [FormsModule, CommonModule, ProductsComponent],
   templateUrl: './homepage.component.html',
   styleUrl: './homepage.component.css'
 })
@@ -22,17 +24,30 @@ export class HomepageComponent {
     {
       name : "Product 1",
       price : 2000,
-      image : "https://edu.sqi.ng/wp-content/uploads/2023/05/software-developer.jpg"
+      image : "https://edu.sqi.ng/wp-content/uploads/2023/05/software-developer.jpg",
+      category : "clothing",
+      isAvailable : false
     },
     {
       name : "Product 2",
       price : 3000,
-      image : "https://edu.sqi.ng/wp-content/uploads/2023/05/hardware-engineer.jpg"
+      image : "https://edu.sqi.ng/wp-content/uploads/2023/05/hardware-engineer.jpg",
+      category : "Recreation",
+      isAvailable : true
     },
     {
       name : "Product 3",
       price : 4000,
-      image : "https://edu.sqi.ng/wp-content/uploads/2024/01/commisioner-for-education-400x250.jpg"
+      image : "https://edu.sqi.ng/wp-content/uploads/2024/01/commisioner-for-education-400x250.jpg",
+      category : "Sport",
+      isAvailable : false
+    },
+    {
+      name : "Product 4",
+      price : 4000,
+      image : "https://edu.sqi.ng/wp-content/uploads/2024/01/commisioner-for-education-400x250.jpg",
+      category : "Foods",
+      isAvailable : true
     }
    ]
 }
---
+++ src/app/products/products.component.ts
@@ -0,0 +1,85 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { OnInit } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-products',
+  standalone: true,
+  imports: [CommonModule, FormsModule],
+  templateUrl: './products.component.html',
+  styleUrl: './products.component.css'
+})
+export class ProductsComponent {
+ 
+  // selectedCourse : string = ""
+  
+   CoursesArray = [
+    {
+      id : 1,
+      name : "Html Complete Course",
+      image : "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS1HVNHQmF6XqXS0xqpvfcJFY3cQIAQEB3XmJ_edOZdMQ&s",
+      description : "  Lorem, ipsum dolor sit amet consectetur adipisicing elit. Error, quasi obcaecati nihil corporis doloremque repellendus omnis perspiciatis? Explicabo, voluptate ullam nesciunt quas at delectus obcaecati expedita blanditiis labore, cupiditate dignissimos.",
+      category : "premium"
+    },
+    {
+      id : 2,
+      name : "Css Complete Course",
+      image : "https://upload.wikimedia.org/wikipedia/commons/d/d5/CSS3_logo_and_wordmark.svg",
+      description : "  Lorem, ipsum dolor sit amet consectetur adipisicing elit. Error, quasi obcaecati nihil corporis doloremque repellendus omnis perspiciatis? Explicabo, voluptate ullam nesciunt quas at delectus obcaecati expedita blanditiis labore, cupiditate dignissimos.",
+      category : "Free"
+    },
+    {
+      id : 3,
+      name : "Javascript Complete Course",
+      image : "https://static.javatpoint.com/images/javascript/javascript_logo.png",
+      description : "  Lorem, ipsum dolor sit amet consectetur adipisicing elit. Error, quasi obcaecati nihil corporis doloremque repellendus omnis perspiciatis? Explicabo, voluptate ullam nesciunt quas at delectus obcaecati expedita blanditiis labore, cupiditate dignissimos.",
+      category : "Free"
+    },
+    {
+      id : 4,
+      name : "Vue Complete Course",
+      image : "https://upload.wikimedia.org/wikipedia/commons/thumb/9/95/Vue.js_Logo_2.svg/1200px-Vue.js_Logo_2.svg.png",
+      description : "  Lorem, ipsum dolor sit amet consectetur adipisicing elit. Error, quasi obcaecati nihil corporis doloremque repellendus omnis perspiciatis? Explicabo, voluptate ullam nesciunt quas at delectus obcaecati expedita blanditiis labore, cupiditate dignissimos.",
+      category : "premium"
+    },
+    {
+      id : 5,
+      name : "React Complete Course",
+      image : "https://www.freecodecamp.org/news/content/images/2021/06/Ekran-Resmi-2019-11-18-18.08.13.png",
+      description : "  Lorem, ipsum dolor sit amet consectetur adipisicing elit. Error, quasi obcaecati nihil corporis doloremque repellendus omnis perspiciatis? Explicabo, voluptate ullam nesciunt quas at delectus obcaecati expedita blanditiis labore, cupiditate dignissimos.",
+      category : "premium"
+    },
+    {
+      id : 6,
+      name : "Angular Complete Course",
+      image : "https://upload.wikimedia.org/wikipedia/commons/thumb/c/cf/Angular_full_color_logo.svg/768px-Angular_full_color_logo.svg.png",
+      description : "  Lorem, ipsum dolor sit amet consectetur adipisicing elit. Error, quasi obcaecati nihil corporis doloremque repellendus omnis perspiciatis? Explicabo, voluptate ullam nesciunt quas at delectus obcaecati expedita blanditiis labore, cupiditate dignissimos.",
+      category : "premium"
+    },
+  ]
+
+  AllCourses  = this.CoursesArray
+
+  PremiumCourses = this.CoursesArray.filter((course)=> course.category === "premium")
+  freeCourses = this.CoursesArray.filter((course)=> course.category === "Free")
+
+  allCourses : number = this.AllCourses.length;
+  premiumCourses : number = this.PremiumCourses.length;
+  FreeCourses : number = this.freeCourses.length
+
+  SelectCourse(course : string){
+    console.log("Selected Course : ", course)
+    if (course === "All Course") {
+       this.CoursesArray = this.AllCourses
+    }else if(course === "Premium Course"){
+      this.CoursesArray = this.PremiumCourses
+    }else if(course === "Free Course"){
+      this.CoursesArray = this.freeCourses
+    }
+  }
+//   onInit(){
+// this.selectedCourse = ""
+//   }
+
+}
